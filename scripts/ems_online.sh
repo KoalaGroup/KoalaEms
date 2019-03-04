a8b3702174fd2e43de242af4af00b1e402248821
Yong Zhou <y.zhou@fz-juelich.de>
2019-03-04 21:25:52 +0100

    [Add] script wrapping koala online program

---
+++ scripts/ems_online.sh
@@ -0,0 +1,18 @@
+# server in the background
+ONLINE_HOME=/home/koala/ems/KoalaEms/build/events++
+
+if [ -d "$ONLINE_HOME" ]; then
+    cd $ONLINE_HOME
+else
+    echo "ERROR: $ONLINE_HOME does not exist!"
+    exit
+fi
+
+# kill existing
+# pkill has a maximum charactor limit
+pkill datacli_koala
+nohup ./datacli_koala localhost:5555 &>/dev/null &
+
+
+pkill online_koala
+nohup ./online_koala &>/dev/null &
